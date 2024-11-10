cf9acce796824a29260483a9d9948a287c72f5ef
Mahmoud <mabdelaziz25899@gmail.com>
2024-11-10 23:50:49 +0200

    Updated Styling for New, Edit and Show pages

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for(let i = 0; i<50; i++){
         const random1000 = Math.floor(Math.random()*1000)
+        const price = Math.floor(Math.random()*20) +10
         const camp = new Campground({ 
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${cities[random1000].city}, ${cities[random1000].state}`
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci blanditiis rerum aspernatur optio placeat maxime nobis! Fugit quam dolorem porro cupiditate libero, veritatis adipisci, repudiandae tempore in deserunt sed consequuntur?',
+            price
         })
         await camp.save()
     }
